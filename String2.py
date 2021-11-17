52a66f6945a60357705ec0d76cba06dbee1c2f52
starli-ai <alifattahi.js@gmail.com>
2021-11-18 01:55:48 +0330

    String2 file v1

---
+++ String2.py
@@ -0,0 +1,14 @@
+# Text interpolation : - String type print : 
+
+username = "Starli.Ai" ; 
+email = "alifattahi.js@gmail.com" ; 
+age = 17  ; 
+
+userInfo = f"Username : {username}  - userEmail {email} - userAge : {age}  "  ; 
+print(userInfo) ; 
+
+# code : f "  {} text {} . . . .  " ; 
+
+# Text indexes  :  get text char ; 
+userEmail = "alifattahi.js@gmail.com" ; 
+print(userEmail[5])
