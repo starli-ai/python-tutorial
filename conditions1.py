1fab0d43b0a26b123923bd35fe325cc316c56a88
starli-ai <alifattahi.js@gmail.com>
2021-11-19 01:05:43 +0330

    Learn Condition Part 1

---
+++ conditions1.py
@@ -0,0 +1,15 @@
+# Python Condition Part 1 : If   elif   else 
+
+userRank = 5 ; 
+
+if userRank == 1 :
+    print('You Got Gold medal . ') ;
+elif userRank == 2 :
+    print('You Got Silver medal') ; 
+elif userRank == 3 : 
+    print('You Got Boronze medal') ; 
+else :  
+    print(' Yoooo dont got medal. ')
+
+
+print('You Are the best !') if userRank == 1 else print('Good Rank. ')
