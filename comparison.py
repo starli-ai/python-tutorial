124603c7f2451d2d6d8ac39733ce65d350ad4bf2
starli-ai <alifattahi.js@gmail.com>
2021-11-24 01:37:17 +0330

    Comparison python

---
+++ comparison.py
@@ -0,0 +1,26 @@
+BTC  = 57300 ; 
+ETH = 4600 ; 
+
+# == equal to 
+print(f' BitCoin:{BTC} == Etherium:{ETH}  :   {BTC == ETH} ') ;
+
+
+# != not equal 
+print(f' BitCoin:{BTC} != Etherium:{ETH}  :   {BTC != ETH} ') ;
+
+# > bigger then 
+print(f' BitCoin:{BTC} => Etherium:{ETH}  :   {BTC > ETH} ') ;
+
+#>= big or equal : 
+print(f' BitCoin:{BTC} >= Etherium:{ETH}  :   {BTC >= ETH} ')
+
+
+# < less than : 
+print(f' BitCoin:{BTC} < Etherium:{ETH}  :   {BTC < ETH} ')
+
+
+# lees or equal : 
+print(f' BitCoin:{BTC} <= Etherium:{ETH}  :   {BTC <= ETH} ')
+
+
+
