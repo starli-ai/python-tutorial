2518eac3bc3f67e9c0e4f1925f52d2603e07e6d0
starli-ai <alifattahi.js@gmail.com>
2021-11-24 22:41:59 +0330

    logical Code - and logical example file

---
+++ example/second.py
@@ -0,0 +1,9 @@
+# Here is Cinema and ticket price : 
+# 2 >=  .... >= 8  is  ==== 2 dollar ; 
+# 65 > is ======== 5 dollar ; 
+# rest is 10 dollar ; 
+
+custumer = 8  ; 
+
+if not (  (custumer >= 2 and  custumer <= 8) or custumer > 65      ) :
+    print('Your ticket price is : 10 $') 
---
+++ logicalOperations.py
@@ -0,0 +1,32 @@
+# Logical Operators : and  - or - not 
+
+
+userAge = 18 ; 
+userJob = "MoneyMerchant" ; 
+
+if userAge >= 18 and userJob == "MoneyMerchant" : 
+    print('You Are very rich men . ') ; 
+else : 
+    print("You need more Try ;") ; 
+
+print ('**********************') ; 
+print (f'True and True {True and True}') ; 
+print (f'True and False {True and False }') ; 
+print(f'False and True {False and True}')
+print(f'False and False {False and False}')
+
+
+
+
+wether = "sunny" ; 
+
+if wether == 'sunny' or wether == 'rainy' : 
+    print('You can travel')
+else : 
+    print('You cant Travel')
+
+print('**********************')
+print(f'True or True {True or True}')
+print(f'True or False {True or False }')
+print(f'False or True {False or True}')
+print(f'False or False {False or False}')
