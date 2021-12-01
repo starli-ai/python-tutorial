0a54dfbb14e4715d3847c796700b6eb4f0f94433
starli-ai <alifattahi.js@gmail.com>
2021-12-01 23:27:27 +0330

    Loop learning Part one

---
+++ loop1.py
@@ -0,0 +1,19 @@
+rangeOfNumber =  [1,2,54,213,874,2983 ,49 , 3 ,19 ,48 ,28 ] ; 
+
+for number_item in rangeOfNumber : 
+    print(f"{number_item}  * 2 => {number_item * 2 } ") ; 
+
+
+username = "starli.ai" ; 
+
+for char in username :
+    print(f"  ({char})  ") ; 
+
+
+
+number_scope = range(7,18)  ; 
+
+for num in number_scope :
+    print(f" {num} ") ; 
+
+ 
